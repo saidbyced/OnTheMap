b9c601bdeb64e4f023016f6ea5caad7a9974c34d
Chris Eadie <chris.eadie@outlook.com>
2020-12-11 17:20:28 +0000

    Begin creation of basic API request structure

--- OnTheMap/API/Request.swift
+++ OnTheMap/API/Request.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+class OnTheMapAPI {
+    static let scheme = "https"
+    static let host = ""
+    static let path = ""
+    
+    enum Params {
+        case limit(Int)
+        case skip(Int)
+        case order(String)
+        case uniqueKey(String)
+        
+        var asQueryItem: URLQueryItem {
+            switch self {
+            case .limit(let limitValue):
+                return URLQueryItem(name: "limit", value: "\(limitValue)")
+            case .skip(let skipValue):
+                return URLQueryItem(name: "skip", value: "\(skipValue)")
+            case .order(let orderValue):
+                return URLQueryItem(name: "order", value: orderValue)
+            case .uniqueKey(let keyValue):
+                return URLQueryItem(name: "", value: keyValue)
+            }
+        }
+    }
+    
+    class func getLocations() {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = OnTheMapAPI.scheme
+        urlComponents.host = OnTheMapAPI.host
+        urlComponents.path = OnTheMapAPI.path
+        urlComponents.queryItems = [
+            OnTheMapAPI.Params.limit(100).asQueryItem
+        ]
+        
+        guard let url = urlComponents.url else {
+            fatalError()
+        }
+        
+        print("\(url)")
+    }
+}
