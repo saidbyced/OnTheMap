1e1323dec995d99ff4cf38033bed7cf24aa02703
Chris Eadie <chris.eadie@outlook.com>
2020-12-14 13:07:13 +0000

    Add response structs for parsing API responses

--- OnTheMap/API/Response.swift
+++ OnTheMap/API/Response.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct StudentLocationsResponse: Codable {
+    let results: [LocationResponse]
+}
+
+struct LocationResponse: Codable {
+    let createdAt: String
+    let firstName: String
+    let lastName: String
+    let latitude: Float
+    let longitude: Double
+    let mapString: String
+    let mediaURL: String
+    let objectId: String
+    let uniqueKey: Int
+    let updatedAt: String
+}
+
+struct CreationResponse: Codable {
+    let createdAt: String
+    let objectId: String
+}
+
+struct UpdateResponse: Codable {
+    let updatedAt: String
+}
