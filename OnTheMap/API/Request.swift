3a4a17530947a5ca6401fcb69b320bd4c233dfea
Chris Eadie <chris.eadie@outlook.com>
2020-12-17 15:32:04 +0000

    Change some enum and variables names for clarity, add UdacityLogin struct for later and create session post request

--- OnTheMap/API/Request.swift
+++ OnTheMap/API/Request.swift
@@ -20,7 +20,7 @@ struct OnTheMapAPI {
         }
     }
     
-    enum Params {
+    enum Query {
         case limit(Int)
         case skip(Int)
         case order(Order)
@@ -54,14 +54,23 @@ struct OnTheMapAPI {
         }
     }
     
+    struct UdacityLogin: Codable {
+        let udacity: LoginDetails
+        
+        struct LoginDetails: Codable {
+            let email: String
+            let password: String
+        }
+    }
+    
     static func getLocations(completion: @escaping ([Location]?, Error?) -> Void) {
         var urlComponents = URLComponents()
         urlComponents.scheme = OnTheMapAPI.scheme
         urlComponents.host = OnTheMapAPI.host
         urlComponents.path = OnTheMapAPI.Path.studentLocation
         urlComponents.queryItems = [
-            OnTheMapAPI.Params.limit(200).asQueryItem,
-            OnTheMapAPI.Params.order(.updatedAtDescending).asQueryItem
+            OnTheMapAPI.Query.limit(200).asQueryItem,
+            OnTheMapAPI.Query.order(.updatedAtDescending).asQueryItem
         ]
         
         guard let url = urlComponents.url else {
@@ -87,10 +96,66 @@ struct OnTheMapAPI {
             }
             
             do {
-                let decodedData = try JSONDecoder().decode(StudentLocationsResponse.self, from: data)
+                let decodedData = try JSONDecoder().decode(LocationsResponse.self, from: data)
                 let locations = decodedData.results
                 DispatchQueue.main.async {
                     completion(locations, nil)
+                }
+            } catch {
+                // FIXME: Handle JSON parsing error
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    static func postSession(username: String, password: String, completion: @escaping (String?, Error?) -> Void) {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = OnTheMapAPI.scheme
+        urlComponents.host = OnTheMapAPI.host
+        urlComponents.path = OnTheMapAPI.Path.session
+        
+        guard let url = urlComponents.url else {
+            // FIXME: Handle url creation error failure
+            print("URL creation failed")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = "{\"udacity\": {\"username\": \"\(username)\", \"password\": \"\(password)\"}}".data(using: .utf8)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                // FIXME: Handle request failure error response
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+            }
+            
+            guard let data = data else {
+                // FIXME: Handle missing data error
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+                return
+            }
+            
+            do {
+                // Remove first 5 characters from data - Udacity security precautions
+                let range = 5..<data.count
+                let skimmedData = data.subdata(in: range)
+                
+                let response = try JSONDecoder().decode(SessionResponse.self, from: skimmedData)
+                let sessionId = response.session.id
+                
+                DispatchQueue.main.async {
+                    completion(sessionId, nil)
                 }
             } catch {
                 // FIXME: Handle JSON parsing error
