ef562ac5501875b9640f32fc3dc78392e92ea2c4
Chris Eadie <chris.eadie@outlook.com>
2020-12-22 12:05:53 +0000

    Add model for holding location prior to posting

---
+++ OnTheMap/Controllers/AddLocationModel.swift
@@ -0,0 +1,13 @@
+//
+//  AddLocationModel.swift
+//  OnTheMap
+//
+//  Created by Chris Eadie on 22/12/2020.
+//
+
+import Foundation
+import MapKit
+
+struct AddLocationModel {
+    static var locationToAdd: OnTheMapAPI.StudentLocationForPosting?
+}
